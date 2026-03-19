b0f4f87f72ae4f1980397fc58610e875c92243ba
prodwillzera <nmare9326@gmail.com>
2026-03-19 11:43:30 -0300

    rework geral do projeto — CSS, HTML e JS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
-const user = document.getElementById('username')
-const password = document.getElementById('password')
+function logar(){
+
+    var login_user = document.getElementById('user').value;
+    var password = document.getElementById('password').value;
+
+    if(login_user == "admin" && password == "admin"){
+        alert('Login realizado!');
+        location.href = "page1.html";
+    }else{
+        alert('Usuario ou senha incorretos.');
+    }
+
+}
+
+const btn = document.querySelector('#submitbtn')
+
+btn.addEventListener("click", function(event){
+    event.preventDefault()
+
+    const day = document.querySelector('#input-day').value;
+    const name = document.querySelector('#input-name').value;
+    const ID = document.querySelector('#input-id').value;
+    const why = document.querySelector('#input-why').value;
+    const sector = document.querySelector('#input-sector').value;
+
+    console.log(day,name,ID,why,sector)
+})
