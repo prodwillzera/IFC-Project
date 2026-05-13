d2115a7d406269fca3e7c80360a197504ecabb2a
prodwillzera <nmare9326@gmail.com>
2026-05-13 11:00:49 -0300

    exibir media geral no grafico

--- src/frontend/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-import { startForm } from './modules/form.js';
-import { applyMask } from './modules/mask.js';
-/* import { validateData, validateHour, validateID } from './modules/validation.js';
-import { saveData } from './modules/api.js'; */
-
-startForm();
-applyMask();
